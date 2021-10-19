4f39263c89630c742fd2db92a6cc0dbc71b97fc7
KuronekoKat <davson82@hotmail.com>
2021-10-19 01:43:46 -0500

    model and properties

---
+++ Proyecto/democrud/src/main/java/com/demo/itscch/democrud/DemocrudApplication.java
@@ -0,0 +1,13 @@
+package com.demo.itscch.democrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocrudApplication.class, args);
+	}
+
+}
---
+++ Proyecto/democrud/src/test/java/com/demo/itscch/democrud/DemocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.itscch.democrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
