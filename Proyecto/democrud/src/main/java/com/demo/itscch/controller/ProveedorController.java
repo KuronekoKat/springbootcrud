39f0bbb8d59032839445c5defd7c39fb4cb96728
KuronekoKat <davson82@hotmail.com>
2021-10-20 13:36:50 -0500

    crud inicial

--- Proyecto/democrud/src/main/java/com/demo/itscch/controller/ProveedorController.java
+++ Proyecto/democrud/src/main/java/com/demo/itscch/controller/ProveedorController.java
@@ -1,10 +1,18 @@
 package com.demo.itscch.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 import com.demo.itscch.model.Proveedor;
 import com.demo.itscch.repository.ProveedorRepository;
@@ -21,7 +29,41 @@ public class ProveedorController {
 	
 	@GetMapping
 	public List<Proveedor> getProveedores(){
+		System.out.println("Llamada a GetProveedores");
 		return proveedorRepository.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public Proveedor getProveedor(@PathVariable Integer id) {
+		System.out.println("Llamada a GetProveedor por ID "+id);
+	return proveedorRepository.findById(id).orElseThrow(RuntimeException::new);
+	}
 
+	@PostMapping
+	public ResponseEntity createProveedor(@RequestBody Proveedor proveedor) throws URISyntaxException{
+		System.out.println("Llamada a crear proveedor");
+		Proveedor savedProveedor = proveedorRepository.save(proveedor);
+		return ResponseEntity.created(new URI("/proveedores/"+savedProveedor.getIdProveedor())).body(savedProveedor);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity updateProveedor(@PathVariable Integer id, @RequestBody Proveedor proveedor) {
+		System.out.println("Llamada a actualizar proveedor");
+		Proveedor currentProveedor = proveedorRepository.findById(id).orElseThrow(RuntimeException::new);
+		currentProveedor.setNombreProveedor(proveedor.getNombreProveedor());
+		currentProveedor.setTelefonoProveedor(proveedor.getTelefonoProveedor());
+		currentProveedor.setDescripcion(proveedor.getDescripcion());
+		currentProveedor = proveedorRepository.save(proveedor);
+		
+		return ResponseEntity.ok(currentProveedor);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity deleteProveedor(@PathVariable Integer id) {
+		System.out.println("Llamada a borrar proveedor");
+		proveedorRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
+	
 }
+
